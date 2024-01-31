2ef27424a07eec34eb54a103b8c0aa6e6ad4faac
Hans <hans.hanany@gmail.com>
2024-01-31 15:29:54 +0100

    Last

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { SirenComponent } from './siren/siren.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        AppRoutingModule,
+        FormsModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        SirenComponent
       ],
     }).compileComponents();
   });
